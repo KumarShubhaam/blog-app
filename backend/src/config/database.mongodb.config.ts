866e642731c8e934941a228d22a62f0adeb1d3d9
KumarShubhaam <shubham59.sst@gmail.com>
2026-04-19 11:59:34 +0530

    added mongodb atlas config to the backend

--- backend/src/config/database.mongodb.config.ts
+++ backend/src/config/database.mongodb.config.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { type ConnectOptions } from "mongoose";
 import dotenv from 'dotenv';
 
 
@@ -14,6 +14,24 @@ async function connectToMongoDB(){
   }
 }
 
+
+async function connectToMongoDBAtlas() {
+  const db_username = encodeURIComponent(process.env.DB_USERNAME || "");
+  const db_password = encodeURIComponent(process.env.DB_PASSWORD || "");
+  const dbUrl = `mongodb+srv://${db_username}:${db_password}@blogapp.xwlc8i6.mongodb.net/?appName=BlogApp`
+  const clientOptions: ConnectOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+  try {
+    await mongoose.connect(dbUrl, clientOptions);
+    await mongoose.connection.db?.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.log(`Database connection failed with error: ${error}`);
+  }
+}
+
+
+
 export {
-  connectToMongoDB
+  connectToMongoDB,
+  connectToMongoDBAtlas
 };
