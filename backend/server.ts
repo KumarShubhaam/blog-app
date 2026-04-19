8fec124c32a177423589852068c18b79874e03a8
KumarShubhaam <shubham59.sst@gmail.com>
2026-04-19 12:01:26 +0530

    updated server.ts for atlas

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectToMongoDB } from './src/config/database.mongodb.config.js';
+import { connectToMongoDB, connectToMongoDBAtlas } from './src/config/database.mongodb.config.js';
 
 dotenv.config()
 
 const app = express()
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, (err) => {
+  if(err) {
+    console.log('App has error:', err);
+  }
   console.log(`Blog App is running on http://localhost:${process.env.PORT}`)
-  connectToMongoDB()
+  connectToMongoDB();
+  // connectToMongoDBAtlas().catch(console.dir);
 })
 
 export default app;
