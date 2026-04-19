ad2bf536eb4ff9fa5c052f75d0539c1a324ec8d0
KumarShubhaam <shubham59.sst@gmail.com>
2026-04-19 12:05:10 +0530

    added cors and auth to backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import app from './server.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import blogRoutes from './src/features/blogs/blogs.routes.js';
 import userRoutes from './src/features/users/users.routers.js';
@@ -8,7 +10,20 @@ import HandleAppError from './src/middlewares/customError.middleware.js';
 
 import { SignUpBulkUser } from './src/features/sample data/users sample/users.sample.js';
 import { createNewBlogs } from './src/features/sample data/blogs sample/blogs.sample.js';
+import Auth from './src/middlewares/auth.middleware.js';
 
+// Cross-origin request policy
+const corsOption = {
+  origin: `http://${process.env.FRONTEND_IP}:${process.env.FRONTEND_PORT}`,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
+}
+
+app.use(cors(corsOption))
+
+// parse incoming cookie
+app.use(cookieParser());
 
 // parse URL encoded data 
 app.use(express.urlencoded({extended:true}));
@@ -19,7 +34,7 @@ app.get('/', (req, res) => {
   res.send('Home page for Blog App')
 })
 
-app.use('/api/blogs', blogRoutes)
+app.use('/api/blogs', Auth ,blogRoutes)
 app.use('/api/users', userRoutes)
 // app.use('/api/comments', blogRoutes)
 
